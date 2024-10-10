df67c7440da029007c1f3d70ea343d800ffdd8c5
hermenegildoAng <hernandezhermenegildoangeldavi@gmail.com>
2024-10-10 09:58:08 -0500

    01 Primer ejercisio

---
+++ ejer1.py
@@ -0,0 +1,15 @@
+#Ejercisio 1: 
+#Escribir un programa que pregunte al usuario nombre e imprima saludo
+#entrdas: nombre: strig
+#salida: saludo: string
+"""
+dkejd
+"""
+
+nomb = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad")
+saludo = "Holaa " + nomb + "de " + edad + " años"
+print(saludo)
+
+
+
